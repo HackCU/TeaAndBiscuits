5ae13e2825636ec383472d65017458f44c3d24f6
Kelly Nicole Kaoudis <kaoudis@colorado.edu>
2015-04-11 15:41:48 -0600

    go go go

--- cordova/www/js/controllers/bus_route_controller.js
+++ cordova/www/js/controllers/bus_route_controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('teaApp')
-  .controller('BusRouteController', function($scope, Storage){
+  .controller('BusRouteController', function($scope, $window, Storage){
     $scope.test = "test";
 
     $scope.submit = function() {
       Storage.setRoute('test', 'testval');
       console.log(Storage.getRoute('test'));
+      $window.location.href = '#/pick-time';
     }
   });
